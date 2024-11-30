392567d130ba11ee65fdd2fdeb996df96e83aaba
Rohan Biju <biju.ro@northeastern.edu>
2024-11-30 17:49:15 -0500

    Update icon.tsx

--- constants/icon.tsx
+++ constants/icon.tsx
@@ -1,9 +1,7 @@
 import { Feather } from "@expo/vector-icons";
 
 export const icon = {
-    index: (props: any) => (
-      <Feather name="message-square" size={24} {...props} />
-    ),
-    events: (props: any) => <Feather name="calendar" size={24} {...props} />,
-    profile: (props: any) => <Feather name="user" size={24} {...props} />,
-  };
+  index: (props: any) => <Feather name="message-square" size={24} {...props} />,
+  events: (props: any) => <Feather name="calendar" size={24} {...props} />,
+  profile: (props: any) => <Feather name="user" size={24} {...props} />,
+};
